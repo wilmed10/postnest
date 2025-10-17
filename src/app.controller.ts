32361afd0d8026ba07d3e40b937d726dfee0f846
Wilson Medina <wilsonmedina8d@gmail.com>
2025-10-16 23:26:28 -0500

    feat: testing http methods at nest controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // asignar url en caso de no ser el principal
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  getPost() {
+    return 'Desde @Post';
+  }
+
+  @Put()
+  getPut() {
+    return 'Desde @Put';
+  }
+
+  @Patch()
+  getPatch() {
+    return 'Desde @Patch';
   }
 }
