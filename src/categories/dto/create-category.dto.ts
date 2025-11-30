ff36c2387ff067228fc2a5a7e5af71aac4ea53ca
Wilson Medina <wilsonmedina8d@gmail.com>
2025-11-29 20:12:47 -0500

    feat: integrate TypeORM for category management and update DTO validation message

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 export class CreateCategoryDto {
-  @IsString({ message: 'El nombre de la categoría no puede ir vacío' })
+  @IsString({ message: 'El nombre de la categoría deb ser un string' })
   @IsNotEmpty({ message: 'El nombre de la categoría no puede estar vacío' })
   name: string;
 }
