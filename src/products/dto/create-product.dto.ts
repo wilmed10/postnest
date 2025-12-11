2227083aa3e67e9c97aeddb9d0da7e359e7f0391
Wilson Medina <wilsonmedina8d@gmail.com>
2025-12-10 22:07:20 -0500

    feat: define products dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+    @IsNotEmpty({message: 'El nombre del producto es obligatorio'})
+    @IsString({message: 'Nombre no válido'})
+    name: string
+
+    @IsNotEmpty({message: 'El precio del producto es obligatorio'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'Precio no válido'})
+    price: number
+
+    @IsNotEmpty({message: 'La cantidad no puede ser vacia'})
+    @IsNumber({maxDecimalPlaces: 2}, {message: 'Cantidad no válida'})
+    inventory : 200
+
+    @IsNotEmpty({message: 'La categoria es obligatoria'})
+    @IsInt({message: 'La categoría no es válida'})
+    categoryId: number
+}
